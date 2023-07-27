af67101931e367638e3a6f8a8bd597b02bb6dc10
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-27 21:29:12 +0400

    [main] Filled up the WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] words = sentence.toLowerCase()
+                .replaceAll("[^a-zA-Z ]+","")
+                .split(" ");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            Integer amount = map.get(word);
+            amount = amount == null ? 0 : amount;
+            map.put(word, amount + 1);
+        }
+
+        return map;
     }
 }
